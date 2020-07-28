6a58fec6bf7d34aa7954afcf8aa669262a1479a1
carolina <carolinaccez@gmail.com>
2020-07-27 21:13:07 -0400

    created src folder and html with name and github name, linked two js files together

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 // console.log('Hello Node!');
 // console.log(document); //this displays error because document is not defined 
 
@@ -12,7 +14,10 @@
 // console.log(commandLineArgs);
 // var profileDataArgs = process.argv.slice(2, process.argv.length);
 // console.log(profileDataArgs);
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+// const name = profileDataArgs[0]; 
+// const github = profileDataArgs[1];
 // console.log(profileDataArgs);
 // const printProfileData = (profileDataArr) => {
 //     console.log(profileDataArr);
@@ -28,12 +33,26 @@
 // };
 
 
-console.log('================');
+// console.log('================');
 
-// profileDataArr.forEach(function (profileItem) {
-//     console.log(profileItem);
-// });
+// // profileDataArr.forEach(function (profileItem) {
+// //     console.log(profileItem);
+// // });
 
-profileDataArr.forEach(profileItem => console.log(profileItem));
+// profileDataArr.forEach(profileItem => console.log(profileItem));
 
-printProfileData(profileDataArr);
+// printProfileData(profileDataArr);
+// const generatePage = () => 'Name: Carolina, Github: carolinacez';
+
+
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+
+
+
+// console.log(name, github);
+// console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
