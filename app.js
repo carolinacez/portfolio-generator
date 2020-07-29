1e52ada43c61b30c881290aa9f30d4ba597222de
carolina <carolinaccez@gmail.com>
2020-07-28 22:24:23 -0400

    learning npm -inquirer

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
 // console.log('Hello Node!');
 // console.log(document); //this displays error because document is not defined 
 
@@ -14,8 +36,10 @@ const generatePage = require('./src/page-template.js');
 // console.log(commandLineArgs);
 // var profileDataArgs = process.argv.slice(2, process.argv.length);
 // console.log(profileDataArgs);
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
+
 // const name = profileDataArgs[0]; 
 // const github = profileDataArgs[1];
 // console.log(profileDataArgs);
@@ -51,8 +75,9 @@ const [name, github] = profileDataArgs;
 
 // console.log(name, github);
 // console.log(generatePage(name, github));
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
